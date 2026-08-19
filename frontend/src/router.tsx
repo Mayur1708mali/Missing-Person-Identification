ec1f6ee1713b9b0fd5e35fb7785220453a26294c
Mayur Mali <mayur1708mali@gmail.com>
2026-08-19 14:12:42 +0530

    feat: add protected Report and Search routes to router

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -2,6 +2,9 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Browse from './pages/Browse';
 import PersonDetail from './pages/PersonDetail';
+import Report from './pages/Report';
+import Search from './pages/Search';
+import ProtectedRoute from './components/ProtectedRoute';
 
 export default function AppRouter() {
   return (
@@ -9,6 +12,22 @@ export default function AppRouter() {
       <Route path="/" element={<Home />} />
       <Route path="/browse" element={<Browse />} />
       <Route path="/person/:id" element={<PersonDetail />} />
+      <Route
+        path="/report"
+        element={
+          <ProtectedRoute>
+            <Report />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/search"
+        element={
+          <ProtectedRoute>
+            <Search />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   );
 }
