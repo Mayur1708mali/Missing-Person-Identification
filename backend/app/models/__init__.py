43ebe7015ffa5fc97ad1ec6b0063eeffab641abf
Mayur Mali <mayur1708mali@gmail.com>
2026-08-19 13:37:31 +0530

    chore: initialize models package

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,5 @@
+from app.models.user import User
+from app.models.missing_person import MissingPerson
+from app.models.face_embedding import FaceEmbedding
+
+__all__ = ["User", "MissingPerson", "FaceEmbedding"]
